465a9703855b681c5c79dbb32c5920029e4e590a
yike Lin <59351525+DirtyWorld-png@users.noreply.github.com>
2020-05-07 17:32:29 +0800

    Add files via upload

---
+++ Brush click sites/Brush click sites/Brush click sites.cpp
@@ -0,0 +1,36 @@
+﻿// Brush click sites.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include<cstdio>               //gets()函数
+#include<cstdlib>              //system()函数
+using namespace std;
+
+int main()
+{
+    cout << "请输入您需要刷的次数 How many hits you need to brush" << endl;
+    int a;
+    cin >> a;
+    cout << "Print Url(请在Url前加上explorer（然后按一个空格并新建一个引号，在引号内输入您的Url（包含http或https））Please add explorer in front of the Url (and then press a space and create a new quotes, enter your Url in quotation marks (including HTTP or HTTPS)))" << endl;
+    char b[500];
+    while(gets_s(b))
+    for (int i = 1; i <= a; i++)
+    {
+        system(b);
+    }
+    cout << "完成 Finish" << endl;
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Brush-click-sites
-一个超简洁（陋）的网站刷点击器
+
+[在这里查看详细信息](http://robblog.xyz/index.php/2020/05/07/%e4%b8%80%e4%b8%aa%e9%9d%9e%e5%b8%b8%e7%ae%80%e6%b4%81%ef%bc%88%e9%99%8b%ef%bc%89%e7%9a%84%e5%88%b7%e7%bd%91%e9%a1%b5%e7%82%b9%e5%87%bb%e5%99%a8%e7%9a%84%e4%b8%9c%e4%b8%9c/)
